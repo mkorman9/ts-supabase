b67e6574b9e305f878b7812059bab5e624527f29
Michal Korman <michal.korman@icloud.com>
2023-11-24 22:53:29 +0100

    use pooling for database connections

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import {createClient} from '@supabase/supabase-js';
 import pg from 'pg';
 
 const supabase = createClient(config.SUPABASE_URL, config.SUPABASE_TOKEN);
-const db = new pg.Client({
+const dbPool = new pg.Pool({
   host: config.DB_HOST,
   port: config.DB_PORT,
   database: config.DB_NAME,
   user: config.DB_USER,
-  password: config.DB_PASSWORD
+  password: config.DB_PASSWORD,
+  max: 5
 });
 
 // Query data from public schema using API
@@ -30,11 +31,11 @@ const db = new pg.Client({
 
 // Query data from private schema using database connection
 (async () => {
-  await db.connect();
-  const rows = await db.query('SELECT id, username from private.users');
+  const conn = await dbPool.connect();
+  const rows = await conn.query('SELECT id, username from private.users');
 
   console.log('Users:');
   rows.rows.forEach(r => console.log(r));
 
-  await db.end();
+  conn.release();
 })();
