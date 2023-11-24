d06da91ca861107205bce54ad0d12c7063e39b26
Michal Korman <michal.korman@icloud.com>
2023-11-24 20:17:52 +0100

    enable row level security for database tables

--- migrate.sh
+++ migrate.sh
@@ -37,6 +37,7 @@ if [ -z "$DB_HOST" ] || [ -z "$DB_PORT" ] || [ -z "$DB_NAME" ] || [ -z "$DB_USER
   exit 1
 fi
 
+# Run migration
 docker run \
   -it \
   --rm \
@@ -46,3 +47,20 @@ docker run \
   -user="${DB_USER}" \
   -password="${DB_PASSWORD}" \
   migrate
+
+# Make sure flyway_schema_history has row level security enabled
+read -r -d '' ENABLE_RLS_QUERY << EOF
+  alter table flyway_schema_history enable row level security;
+  create policy flyway_schema_history_policy on flyway_schema_history for ALL to anon using (false);
+EOF
+
+docker run \
+  -it \
+  --rm \
+  -e PGPASSWORD="${DB_PASSWORD}" \
+  postgres:15 \
+    psql \
+    -h "${DB_HOST}" \
+    -U "${DB_USER}" \
+    "${DB_NAME}" \
+    -c "${ENABLE_RLS_QUERY}" &> /dev/null || true
--- migrations/V1.0.0__init.sql
+++ migrations/V1.0.0__init.sql
@@ -1,4 +1,25 @@
+-- disable public access
+revoke all privileges on database postgres from anon;
+revoke all privileges on schema public from anon;
+revoke all privileges on schema storage from anon;
+revoke all privileges on all sequences in schema public from anon;
+revoke all privileges on all sequences in schema storage from anon;
+revoke all privileges on all functions in schema public from anon;
+revoke all privileges on all functions in schema storage from anon;
+revoke all privileges on all tables in schema public from anon;
+revoke all privileges on all tables in schema storage from anon;
+
+revoke all on all routines in schema public from public;
+
+-- todo_items
 create table todo_items (
     id         uuid          constraint todo_items_pkey primary key,
     content    varchar(255)
 );
+
+alter table todo_items enable row level security;
+create policy todo_items_policy
+    on todo_items
+    for ALL
+    to anon
+    using (false);
