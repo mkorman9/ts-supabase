391abbdec09b498a0a93772353ad8e6175172414
Michal Korman <michal.korman@icloud.com>
2023-11-25 01:07:44 +0100

    format migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -38,7 +38,12 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-if [ -z "$DB_HOST" ] || [ -z "$DB_PORT" ] || [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ]; then
+if [ -z "$DB_HOST" ] \
+  || [ -z "$DB_PORT" ] \
+  || [ -z "$DB_NAME" ] \
+  || [ -z "$DB_USER" ] \
+  || [ -z "$DB_PASSWORD" ] \
+  || [ -z "$DB_SCHEMA" ]; then
   echo 'usage: ./migrate.sh --host <HOST> --port <PORT> --db <DB_NAME> --user <USER> --password <PASSWORD>'
   exit 1
 fi
